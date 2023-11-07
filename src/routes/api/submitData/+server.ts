051438c3f9c063a5adb701df6066f8a5ae0d9d8d
Tobias Buchner <tobias.buchner@htl-braunau.at>
2023-11-07 01:10:01 +0100

    Fixed Fs error, now saves Link into a file

--- src/routes/api/submitData/+server.ts
+++ src/routes/api/submitData/+server.ts
@@ -1,51 +1,61 @@
 import * as fs from 'fs/promises';
 import path from 'path';
 
-export async function POST({request}) {
-   
-    try {
+export async function POST({ request }: { request: Request }) {
+  try {
+    if (request.body !== null) {
+      const convertedString = await streamToString(request.body);
 
-        const formData = JSON.parse(request.body) || {};
-        console.log("form:" + formData);
-        let test = JSON.stringify(formData, null, 2)
-        console.log(test);
-        
-        const filename = `data_${Date.now()}.json`;
-        
-        const moduleURL = new URL(import.meta.url);
-        let modulePath = moduleURL.pathname;
-    
-        if (process.platform === 'win32') {
-            modulePath = modulePath.replace(/^\/(?=[A-Z]:)/, '');
-            modulePath = decodeURIComponent(modulePath); 
-        }
-    
-        const dataFolder = path.join(path.dirname(modulePath), '../../../json/files');
-        
-        await fs.mkdir(dataFolder, { recursive: true });
-    
-        const filePath = path.join(dataFolder, filename);
-    
-        await fs.writeFile(filePath, JSON.stringify(formData, null, 2));
-    
-    
+      const formData = JSON.parse(convertedString) || {};
+      console.log("form:" + formData);
+      let test = JSON.stringify(formData, null, 2);
+      console.log(test);
 
-        return {
-            status: 200,
-            body: { message: 'Data submitted successfully' },
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-    } catch (error) {
-        console.error(error);
-        return {
-            status: 500,
-            body: { message: 'Internal server error' },
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
+      const filename = `data_${Date.now()}.json`;
+
+      const moduleURL = new URL(import.meta.url);
+      let modulePath = moduleURL.pathname;
+
+      if (process.platform === 'win32') {
+        modulePath = modulePath.replace(/^\/(?=[A-Z]:)/, '');
+        modulePath = decodeURIComponent(modulePath);
+      }
+
+      const dataFolder = path.join(path.dirname(modulePath), '../../../json/files');
+
+      await fs.mkdir(dataFolder, { recursive: true });
+
+      const filePath = path.join(dataFolder, filename);
+
+      await fs.writeFile(filePath, JSON.stringify(formData, null, 2));
     }
+
+    return {
+      status: 200,
+      body: { message: 'Data submitted successfully' },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      status: 500,
+      body: { message: 'Internal server error' },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  }
 }
 
+async function streamToString(stream: ReadableStream<Uint8Array>): Promise<string> {
+  const reader = stream.getReader();
+  let result = '';
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done) break;
+    result += new TextDecoder().decode(value);
+  }
+  return result;
+}
